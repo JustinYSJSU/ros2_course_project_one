f21279be22b2c28ccdb98d81d9350d253830c889
Justin Yamamoto <justinkaiy02@gmail.com>
2026-03-24 11:23:29 -0700

    Add node parameter

--- src/project_one/scripts/combined.py
+++ src/project_one/scripts/combined.py
@@ -2,16 +2,17 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 
-WHEEL_RADIUS = 12.5
+WHEEL_RADIUS_DEFAULT = 12.5
 class CombinedRPM(Node):
     def __init__(self):
         super().__init__("combined_rpm_node")
         self.sub = self.create_subscription(Float32, "rpm", self.calculate_speed, 10)
         self.pub = self.create_publisher(Float32, "speed", 15)
+        self.declare_parameter("wheel_radius", WHEEL_RADIUS_DEFAULT) # new addition, set wheel_radius as an actual node parameter
 
     def calculate_speed(self, rpm_msg):
         print(f"RPM: {rpm_msg.data}")
-        speed = rpm_msg.data * WHEEL_RADIUS * 2 * 3.14 / 60
+        speed = rpm_msg.data * self.get_parameter("wheel_radius").get_parameter_value().double_value * 2 * 3.14 / 60
         msg = Float32()
         msg.data = float(speed)
         self.pub.publish(msg)
